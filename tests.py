1dd8e73676dace7a07254bab2333b6716e85dd08
jake-bickle <bickle.jake@gmail.com>
2018-12-12 12:41:56 -0800

    Few test headers

--- tests.py
+++ tests.py
@@ -1,5 +1,15 @@
 import unittest
 import recon
 
+class test_scraper(unittest.TestCase):
+    #TODO
+    def test_scrape_href(self):
+        pass    
+
+    def test_scrape_email(self):
+        pass
+
+
+
 if __name__ == "__main__":
     unittest.main()
