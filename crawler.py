07354e3533b1a782b7f976065992aefec6981389
jake-bickle <bickle.jake@gmail.com>
2018-12-30 19:18:18 -0800

    Somewhat working version of crawler created

--- crawler.py
+++ crawler.py
@@ -1,11 +1,13 @@
 import requests
 import urlparser
 import timewidgets
+import json
 
 from scheduler import Scheduler
 from scraper import Scraper
-from objdict import ObjDict
 from enum import Enum
+
+import pdb
 
 class Amount(Enum):
     NONE = 0
@@ -68,42 +70,31 @@ class CrawlerConfig:
         self.crawler_delay = Amount.none 
         self.fuzz_level = Amount.HIGH
 
-# I would like to do this, but ObjDict doesn't seem to work completely? dumps() suppress or exclude_nulls doesn't work
-# class netloc(ObjDict):
-    # __keys__ = "name path"
-    # def __init__(self, name, path = list()):
-        # self.name = name
-        # self.path = path
-
 # TODO: Fix: New subdomains won't have fuzz or robots added 
 class Crawler:
-    def __init__(self, seed="https://www.calcharter.com", config=CrawlerConfig()):
+    def __init__(self, seed, config=CrawlerConfig()):
         self.config = config
+        self.seed = seed
         self.output = list()
 
-    # def crawl_next(self):
-        # url_to_crawl = schedular.next_url()
-        # request = requests.get(url_to_crawl, headers={})
-        # page_data = Scraper(request.text, "html.parser")
-
-    def crawl(seed):
-        self.scheduler = Scheduluer(seed)
-        sw = timewidgets.Stopwatch(self.config.crawler_delay)
+    def crawl(self):
+        self.scheduler = Scheduler(self.seed)
+        sw = timewidgets.Stopwatch(0) # TODO: make work with enum self.config.crawler_delay
         timer = timewidgets.Timer()
         timer.start()
         next_url = self.scheduler.next_url()
         while (next_url):
             sw.start()
-            if timer.elapsed() > 30:
+            if timer.elapsed() > 5:
+                self.output_to_file("arachnid_data.json")
                 timer.restart()
-                # Overwrite json file
             self.crawl_page(next_url)
             next_url = self.scheduler.next_url()
             sw.wait()  # Delay the crawler to throw off automated systems
-        # Overwrite output one last time
+        self._write_to_file("arachnid_data.json")
 
-
-    def crawl_page(url):
+    def crawl_page(self, url):
+        print(url)
         p_url = urlparser.parse_url(url)
         netloc_output = self._get_netloc_from_output(p_url.get_netloc())
         if netloc_output is None:
@@ -112,56 +103,55 @@ class Crawler:
             netloc_output["pages"] = list()
             netloc_output["documents"] = list()
             self.output.append(netloc_output)
-        request = requests.get(url_to_crawl, headers={}) # TODO Adjust header info
+        request = requests.get(url, headers={"User-Agent": self.config.agent}) 
         if "text/html" not in request.headers["content-type"]:
-            doc_output = dict()
-            doc_output["path"] = p_url.path
-            cd = request.headers["content-disposition"]
-            f_s = cd.find("filename")+10
-            f_e = cd.find("\"", filename_s)
-            doc_output["name"] = cd[f_s:f_e]  # filename must be parsed from content-disposition
-            doc_output["type"] = request.headers["content-type"]
-            netloc_output["documents"].append(doc_output)
+            pass
+            # doc_output = dict()
+            # doc_output["path"] = p_url.path
+            # cd = request.headers["content-disposition"]
+            # f_s = cd.find("filename")+10
+            # f_e = cd.find("\"", filename_s)
+            # doc_output["name"] = cd[f_s:f_e]  # filename must be parsed from content-disposition
+            # doc_output["type"] = request.headers["content-type"]
+            # netloc_output["documents"].append(doc_output)
         else:
-            page_data = Scraper(r.text)
-            netloc_output["pages"].append(self.extract_page_data(page_data))
+            page_data = Scraper(request.text, "html.parser")  #TODO May change to different parser
             anchors = page_data.find_all("a")
             for a in anchors:
-                if a.href:
-                    url = urlparser.parse_url(a.href)
+                try:
+                    new_url = urlparser.join_url(url, a["href"])
+                    self.scheduler.schedule_url(new_url)
+                except AttributeError:
+                    pass  # If the anchor has no href, move on
+            page_dict = self._extract_page_data(page_data)
+            page_dict["path"] = p_url.path
+            netloc_output["pages"].append(page_dict)
 
-    def _write_output():
-        with open("crawled_site.json", "w") as output:
-            pass
+    def _get_netloc_from_output(self, netloc):
+        for dictionary in self.output:
+            if dictionary["netloc"] == netloc:
+                return dictionary
+        return None
+
+    def output_to_file(self,  filename):
+        with open(filename, "w") as f:
+            data = json.dumps(self.output, indent=4)
+            f.write(data)
             
-    def scrape_page(page_contents):
-        page_data = ObjDict()
-        page_data.name = path
-
-        if (config.scrape_email):
-            page_data.email = page_contents.find_all_emails(page_contents)
-        if (config.scrape_subdomains):
-            pass
-        if (config.scrape_phone_number):
-            page_data.phone = page_contents.find_all_phone_numbers(page_contents)
-        if (config.scrape_social_media):
-            page_data.social = page_contents.find_all_socials(page_contents)
-        if (config.scrape_common_docs):
-            if (config.custom_doc is None):
-                page_data.document = page_contents.find_all_documents()
+    def _extract_page_data(self, page_contents):
+        page_data = dict()
+        if (self.config.scrape_email):
+            page_data["email"] = page_contents.find_all_emails()
+        if (self.config.scrape_phone_number):
+            page_data["phone"] = page_contents.find_all_phones()
+        if (self.config.scrape_social_media):
+            page_data["social"] = page_contents.find_all_social()
+        if (self.config.custom_str):
+            if self.config.custom_str_case_sensitive:
+                page_data["custom_string_occurances"] = page_contents.string_occurances(self.config.custom_str, case_sensitive=True)
             else:
-                pass
-        if (config.custom_str is not None):
-            #TODO config needs to gather custom_str
-            if config.custom_str_case_sensitive:
-                page_data.custom_string_occurance = page_contents.has_string_occurance("I NEED SOMETHING", case_sensitive=True)
-            else:
-                page_data.custom_string_occurance = page_contents.has_string_occurance("I NEED SOMETHING", case_sensitive=False)
-
-        if (config.custom_regex is not None):
-            pass
-
-
-    def generate_fuzz_list():
-        pass
+                page_data["custom_string_occurances"] = page_contents.string_occurances(self.config.custom_str, case_sensitive=False)
+        if (self.config.custom_regex):
+            page_data["custom_regex"] = page_contents.find_all_regex(self.config.custom_regex)
+        return page_data
 
