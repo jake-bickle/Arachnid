b3dcb77fa4c63b33e021fd87cf15464a91f8ad2a
jake-bickle <bickle.jake@gmail.com>
2019-01-03 13:14:36 -0800

    Change: Refactor DomainBlock to keep track of URL extensions, rather than only paths

--- scheduler.py
+++ scheduler.py
@@ -1,37 +1,41 @@
 import urlparser
 from collections import deque
 
-import pdb
-
 class DomainBlock:
-    """ Holds a stack of paths to be crawled for an arbitrary net location """
-    def __init__(self, parsed_url):
-        self.parsed_url = parsed_url
-        self.paths_to_crawl = deque()  # To be used as a stack
-        if not self.parsed_url.path:
-            self.paths_to_crawl.append("/")
-        else:
-            self.paths_to_crawl.append(parsed_url.path)
+    """ Holds a stack of extensions to be crawled for an arbitrary net location 
+        An extension is the portion of the URL that occurs after the suffix
 
-    def add_path(self, path):
-        if self.path_already_added(path):
+        https://www.example.com/path/to/location;key1=value1?key2=value2#content
+                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    """
+    def __init__(self, parsed_url):
+        self.base = parsed_url.get_base()
+        self.extensions_to_crawl = deque()  # To be used as a stack
+        if parsed_url.get_extension():
+            self.add_extension(parsed_url.get_extension())
+        else:
+            self.add_extension("/")
+
+    def add_extension(self, ext):
+        if self.extension_already_added(ext):
             return False
-        self.paths_to_crawl.append(path)
+        self.extensions_to_crawl.append(ext)
         return True
 
     def next_url(self):
         try:
-            path = self.paths_to_crawl.pop()
+            ext = self.extensions_to_crawl.pop()
         except(IndexError):
             return None
-        url = urlparser.join_url(self.parsed_url.get_url(), path)
+        url = urlparser.join_url(self.base, ext) 
         return url
 
-    def path_already_added(self, path):
-        return path in self.paths_to_crawl
+    def extension_already_added(self, extension):
+        return extension in self.extensions_to_crawl
 
 class Scheduler:
     """ Holds a queue of DomainBlocks for an arbitrary domain """
+
     def __init__(self, url=""):
         self.blocks_to_crawl = deque()  # To be used as a queue
         self.crawled_urls = set()
@@ -45,9 +49,9 @@ class Scheduler:
             - It has already been scheduled
         """
         parsed_url = urlparser.parse_url(url)
-        if self.url_has_been_crawled(url):
-            return False
         if not urlparser.same_domain(parsed_url, self.seed_url):
+            return False
+        if self.has_been_crawled(url):
             return False
         block = self._get_domain_block(parsed_url)
         if block is None:
@@ -55,24 +59,25 @@ class Scheduler:
             self.blocks_to_crawl.append(block)
             return True
         else:
-            return block.add_path(parsed_url.get_extension())
+            return block.add_extension(parsed_url.get_extension())
 
     def next_url(self):
         if not self.blocks_to_crawl: 
             return None
         block_to_crawl = self.blocks_to_crawl[0]
         url = block_to_crawl.next_url()
-        if not block_to_crawl.paths_to_crawl:
+        if not block_to_crawl.extensions_to_crawl:
             self.blocks_to_crawl.popleft()
         self.crawled_urls.add(url)
         return url
 
-    def url_has_been_crawled(self, url):
+    def has_been_crawled(self, url):
         return url in self.crawled_urls
 
     def _get_domain_block(self, url):
         for block in self.blocks_to_crawl:
-            if block.parsed_url.subdomain == url.subdomain:
+            b_sub = urlparser.parse_url(block.base).subdomain
+            if b_sub == url.subdomain:
                 return block
         return None
 
