bd824f4c4bf72f187eff06258dc8bf3ba971a478
jake-bickle <bickle.jake@gmail.com>
2019-02-08 19:47:56 -0800

    Change to match new sample_output

--- src/domaindata.py
+++ src/domaindata.py
@@ -15,52 +15,47 @@ class DomainData:
         p_n = tldextract.extract(netloc)
         self.domain = p_n.domain
         self.suffix = p_n.suffix
-        self.subdomain_data = list()
+        # The majority of the data is held within sets to prevent duplicate data.
+        self.data = { "sites" : list(),
+                      "phone_numbers" : set(),
+                      "emails" : set(),
+                      "social_media": set(),
+                      "custom_regex" : set()}
         self._new_subdomain(netloc)
         
     def dumps(self, **kwargs):
-        return json.dumps(self.subdomain_data, cls=SetToList, **kwargs)
+        return json.dumps(self.data, cls=SetToList, **kwargs)
 
     def add_page(self, netloc, page):
         sub = self._ensure_subdomain(netloc)
         sub["pages"].append(page)
 
     def add_phone(self, netloc, phone_number):
-        sub = self._ensure_subdomain(netloc)
-        sub["phone_numbers"].add(phone_number)
+        self.data["phone_numbers"].add(phone_number)
 
     def add_email(self, netloc, email):
-        sub = self._ensure_subdomain(netloc)
-        sub["emails"].add(email)
+        self.data["emails"].add(email)
 
     def add_social(self, netloc, social_media):
-        sub = self._ensure_subdomain(netloc)
-        sub["social_media"].add(social_media)
+        self.data["social_media"].add(social_media)
 
     def add_document(self, netloc, document):
         sub = self._ensure_subdomain(netloc)
         sub["documents"].append(document)
 
     def add_custom_regex(self, netloc, regex):
-        sub = self._ensure_subdomain(netloc)
-        sub["regex"].add(regex)
+        self.data["regex"].add(regex)
 
     def _new_subdomain(self, netloc):
-        # The majority of the data is held within sets to prevent duplicate data.
         self.throw_if_wrong_domain(netloc)
-        sub = {"netloc": netloc,
+        sub = { "netloc": netloc,
                 "pages": list(),        # pages and documents will never get duplicate data, as the crawler will
-                "documents": list(),    # never crawl the same link twice
-                "emails": set(),
-                "phone_numbers": set(),
-                "social_media": set(),
-                "custom_regex": set(),
-                }
-        self.subdomain_data.append(sub)
+                "documents": list() }   # never crawl the same link twice. Meaning a set is not necessary
+        self.data["sites"].append(sub);
         return sub
 
     def _retrieve_subdomain(self, netloc):
-        for sub in self.subdomain_data:
+        for sub in self.data["sites"]:
             if sub["netloc"] == netloc:
                 return sub
         return None
