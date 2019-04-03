b21c684485054327a9c2c0a582417a511899adbf
jake-bickle <bickle.jake@gmail.com>
2019-04-03 16:07:59 -0700

    Remove HTMLResponse as it's unnecessary

--- src/responseparser.py
+++ src/responseparser.py
@@ -1,6 +1,6 @@
 import mimetypes
 import urlparser
-from scraper import Scraper
+
 
 def _get_matching_element(a, b):
     for x in a:
@@ -8,6 +8,7 @@ def _get_matching_element(a, b):
             if x == y:
                 return x
     return None
+
 
 class DocumentResponse:
     def __init__(self, response, applicable_types):
@@ -33,28 +34,4 @@ class DocumentResponse:
         except KeyError:
             path = urlparser.parse_url(self.response.url).path
             return path.split("/")[-1]
-
-class HTMLResponse:
-    def __init__(self, response, config):
-        self.response = response
-        self.config = config
-
-    def extract(self):
-        data = dict()
-        page_contents = Scraper(self.response.text, "html.parser")
-        if (self.config.scrape_email):
-            data["email"] = page_contents.find_all_emails()
-        if (self.config.scrape_phone_number):
-            data["phone"] = page_contents.find_all_phones()
-        if (self.config.scrape_social_media):
-            data["social"] = page_contents.find_all_social()
-        if (self.config.custom_str):
-            if self.config.custom_str_case_sensitive:
-                data["custom_string_occurances"] = page_contents.string_occurances(self.config.custom_str, case_sensitive=True)
-            else:
-                data["custom_string_occurances"] = page_contents.string_occurances(self.config.custom_str, case_sensitive=False)
-        if (self.config.custom_regex):
-            data["custom_regex"] = page_contents.find_all_regex(self.config.custom_regex)
-        data["code"] = self.response.status_code
-        return data
 
