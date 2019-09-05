9ec480fbdd0f8b91be227eb737f6847f91c5d090
jake-bickle <bickle.jake@gmail.com>
2019-09-04 18:03:52 -0700

    Fix a few modules not being added to PYTHONPATH and failing to import outside of PyCharm

--- arachnid/timewidgets.py
+++ arachnid/timewidgets.py
@@ -1,5 +1,6 @@
 import time
 
+
 class Stopwatch:
     def __init__(self, seconds=0):
         self.time_limit = seconds
@@ -15,6 +16,7 @@ class Stopwatch:
         remaining_time = self.time_limit - elapsed_time
         if remaining_time > 0:
             time.sleep(remaining_time)
+
 
 class Timer:
     def start(self):
