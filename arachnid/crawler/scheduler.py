1e15fa3faf0ea9f21bf3cafda11552b34cc62c42
jake-bickle <bickle.jake@gmail.com>
2019-09-25 14:21:41 -0700

    Cleaned up some code and comments

--- arachnid/crawler/scheduler.py
+++ arachnid/crawler/scheduler.py
@@ -12,22 +12,18 @@ FuzzingOptions = namedtuple("FuzzingOptions", ["paths_list_loc", "subs_list_loc"
 
 
 class Scheduler:
-    """ Holds a queue of DomainBlocks for an arbitrary domain """
-
     def __init__(self, c_url, useragent="", fuzzing_options=None, respect_robots=True, allow_subdomains=True,
                  blacklist_dirs=[]):
-        """ Sets up the scheduler to send out by a queue of domainblocks (aka a subdomain).
-            Each domainblock will send out all of its available c_urls before being removed from queue.
-        """
         self.seed = c_url
-        self.previous_c_url = None
+        self.prev_c_url = None
+        self.prev_c_url_is_supplemental = False
         self.bank = {c_url: 100000}
         self.supplemental_c_url_queue = deque()  # A URL from robots.txt or Fuzz of any kind
-        self.virtual_cache = 0
+        self.virtual_credit = 0
         self.allow_subdomains = allow_subdomains
         self.blacklist_dirs = set(blacklist_dirs)
         self.respect_robots = respect_robots
-        self.robot_db = dict()  # Key netloc string: Value RobotFileParser object
+        self.robot_cache = dict()  # Key netloc string: Value RobotFileParser object
         self.useragent = useragent
         self.current_delay = 0
         self.paths_to_fuzz = []
@@ -35,11 +31,11 @@ class Scheduler:
         if fuzzing_options:
             if fuzzing_options.paths_list_loc:
                 with open(fuzzing_options.paths_list_loc) as f:
-                    self.paths_to_fuzz = set("/" + line.strip() for line in f)  # Add to supplemental on a thread
+                    self.paths_to_fuzz = set("/" + line.strip() for line in f)
                 self.fuzz_paths = True
             if fuzzing_options.subs_list_loc:
                 with open(fuzzing_options.subs_list_loc) as f:
-                    self.subs_to_fuzz = set(line.strip() for line in f)  # Add to supplemental on a thread
+                    self.subs_to_fuzz = set(line.strip() for line in f)
                 threading.Thread(target=self._add_sub_fuzz_to_sq())
         self._add_new_subdomain(self.seed)
 
@@ -48,62 +44,68 @@ class Scheduler:
         This performs a modified version of the AOPIC algorithm. Refer to the following issue for more info:
         https://github.com/jake-bickle/Arachnid/issues/6
         """
-        if self.previous_c_url.is_fuzzed() or self.previous_c_url.in_robots():
-            # Slight mod to AOPIC: Any supplemental previous_c_url is given 1000 cache
-            cache = 1000
+        if self.prev_c_url_is_supplemental:
+            # Slight mod to AOPIC: Any supplemental prev_c_url is given 1000 credits
+            creds = 1000
         else:
-            cache = self.bank[self.previous_c_url]
-            self.bank[self.previous_c_url] = 0
-        cleaned_urls = [c_url for c_url in found_c_urls if self.will_schedule(c_url)]
-        tax = cache / 10
-        self.virtual_cache += tax
-        if len(cleaned_urls) > 0:
-            cache_per_page = (cache - tax) / len(cleaned_urls)
-            for c_url in cleaned_urls:
-                if c_url.get_netloc() not in self.robot_db.keys():
+            creds = self.bank[self.prev_c_url]
+            self.bank[self.prev_c_url] = 0
+        sanitized_urls = [c_url for c_url in found_c_urls if self.will_schedule(c_url)]
+        tax = creds / 10
+        self.virtual_credit += tax
+        if len(sanitized_urls) > 0:
+            credits_per_page = (creds - tax) / len(sanitized_urls)
+            for c_url in sanitized_urls:
+                if c_url.get_netloc() not in self.robot_cache.keys():
                     self._add_new_subdomain(c_url)
                 try:
-                    # Slight mod to AOPIC: Do not give credit to any c_url with 0 cache. This has already been crawled
+                    # Slight mod to AOPIC: Do not give credit to any c_url with 0 credits. This has already been crawled
                     if self.bank[c_url] != 0:
-                        self.bank[c_url] += cache_per_page
+                        self.bank[c_url] += credits_per_page
                 except KeyError:
-                    self.bank[c_url] = cache_per_page
+                    # c_url isn't in the bank, add it
+                    if c_url.get_url_parts().path in self.paths_to_fuzz:
+                        c_url.set_on_fuzz(True)
+                    self.bank[c_url] = credits_per_page
         num_uncrawled = len(tuple(c_url for c_url, credit in self.bank.items()))
-        if max(self.bank.values()) < self.virtual_cache and num_uncrawled != 0:
-            bonus = self.virtual_cache / num_uncrawled
+        if max(self.bank.values()) < self.virtual_credit and num_uncrawled != 0:
+            bonus = self.virtual_credit / num_uncrawled
             for c_url, credit in self.bank.items():
                 if credit != 0:
                     self.bank[c_url] += bonus
-            self.virtual_cache = 0
+            self.virtual_credit = 0
 
     def next_url(self):
         try:
             c_url = self.supplemental_c_url_queue.pop()
+            self.prev_c_url_is_supplemental = True
         except IndexError:
             c_url = max(self.bank, key=self.bank.get)
+            self.prev_c_url_is_supplemental = False
             if self.bank[c_url] == 0:
-                # Since a c_url with a cache of 0 means its been crawled, then all c_urls have been crawled
+                # Since a c_url with 0 credits means it has been crawled, then all c_urls have been crawled
                 return None
-        if c_url.get_url_parts().path in self.paths_to_fuzz:
-            c_url.set_on_fuzz(True)
         self.current_delay = self.calculate_crawl_delay(c_url)
-        self.previous_c_url = c_url
+        self.prev_c_url = c_url
         return c_url
 
     def get_crawl_delay(self):
         return self.current_delay
 
     def will_schedule(self, c_url):
-        """ Returns whether a given c_url will be scheduled or not """
+        """ Returns whether a c_url will be scheduled or not. The rules are as follows:
+        - c_url must be a subdomain of the seed c_url
+        - If subdomains are not allowed, then c_url must have the same netloc as the seed c_url
+        - If respecting robots, the c_url along with the scheduler's useragent must be allowed as per the subdomain's
+          robots.txt rules
+        - The c_url path must not have a blacklisted directory
+        """
         if not url_functions.is_subdomain(c_url, self.seed):
-            # Must always be a subdomain
             return False
         if not self.allow_subdomains and not self.seed.get_netloc() == c_url.get_netloc():
-            # If subdomains is not allowed, then c_url must be of the same netloc
             return False
         if self.respect_robots:
-            # Don't crawl if robots.txt disallows it and scheduler is respecting it
-            robots = self._ensure_robots(c_url)
+            robots = self._get_robots(c_url)
             if not robots.can_fetch(self.useragent, c_url.get_url()):
                 return False
         for dir in c_url.get_url_parts().path.split("/"):
@@ -113,7 +115,7 @@ class Scheduler:
 
     def calculate_crawl_delay(self, c_url):
         if self.respect_robots:
-            robots = self._ensure_robots(c_url)
+            robots = self._get_robots(c_url)
             if robots.crawl_delay(self.useragent):
                 return robots.crawl_delay(self.useragent)
             elif robots.request_rate(self.useragent):
@@ -122,7 +124,7 @@ class Scheduler:
         return 0
 
     def _add_new_subdomain(self, c_url):
-        robots = self._ensure_robots(c_url)
+        robots = self._get_robots(c_url)
         if not self.respect_robots:
             for path in robots.all_paths():
                 new_url = url_functions.join_url(c_url.get_base(), path)
@@ -164,11 +166,11 @@ class Scheduler:
                 if not e.__class__.__name__ == "ConnectionError":
                     warning_issuer.issue_warning_from_exception(e, sub_to_check.get_url())
 
-    def _ensure_robots(self, c_url):
+    def _get_robots(self, c_url):
         try:
-            return self.robot_db[c_url.get_netloc()]
+            return self.robot_cache[c_url.get_netloc()]
         except KeyError:
             new_sub_robots = RobotFileParser(url_functions.get_robots(c_url))
             new_sub_robots.read()
-            self.robot_db[c_url.get_netloc()] = new_sub_robots
+            self.robot_cache[c_url.get_netloc()] = new_sub_robots
             return new_sub_robots
