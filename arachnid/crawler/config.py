e7370fa6c5f79c3ba9ec98271f57560ef8bfae8a
jake-bickle <bickle.jake@gmail.com>
2019-10-10 12:49:29 -0700

    Move CrawlerConfig to separate module, config

---
+++ arachnid/crawler/config.py
@@ -0,0 +1,50 @@
+import arachnid_enums
+
+
+class CrawlerConfig:
+    def __init__(self):
+        self.set_default()
+
+    def set_default(self):
+        self.__init__()
+        self.scrape_links = True
+        self.scrape_subdomains = True
+        self.scrape_phone_number = True
+        self.scrape_email = True
+        self.scrape_social_media = True
+        self.documents = {"doc", "docx", "ppt", "pptx", "pps", "xls", "xlsx", "csv", "odt", "odp", "pdf", "txt",
+                          "zip", "rar", "dmg", "exe", "apk", "bin", "rpm", "dpkg"}
+        self.obey_robots = True
+        self.allow_query = True
+        self.agent = arachnid_enums.Agent.FIREFOX.value
+        self.custom_str = None
+        self.custom_str_case_sensitive = False
+        self.custom_regex = None
+        self.default_delay = arachnid_enums.Delay.NONE.value
+        self.paths_list_file_loc = os.path.join(this_dir, "data/fuzz_list.txt")
+        self.subs_list_file_loc = os.path.join(this_dir, "data/subdomain_fuzz_list.txt")
+        self.fuzz_paths = False
+        self.fuzz_subs = False
+        self.blacklisted_directories = []
+
+    def set_stealth(self):
+        self.obey_robots = True
+        self.agent = arachnid_enums.Agent.GOOGLE.value
+        self.default_delay = arachnid_enums.Delay.HIGH.value
+        self.fuzz_paths = False
+        self.fuzz_subs = False
+
+    def set_aggressive(self):
+        self.obey_robots = False
+        self.default_delay = arachnid_enums.Delay.NONE.value
+        self.fuzz_paths = True
+        self.fuzz_subs = True
+
+    def set_layout_only(self):
+        self.scrape_subdomains = False
+        self.scrape_phone_number = False
+        self.scrape_email = False
+        self.scrape_social_media = False
+        self.documents = {}
+        self.custom_str = None
+        self.custom_regex = None
--- arachnid/crawler/crawler.py
+++ arachnid/crawler/crawler.py
@@ -1,9 +1,9 @@
 import requests
-import arachnid_enums
 import random
 import os
 from timewidgets import Timer
 
+from . import config
 from . import responseparser
 from .scheduler import Scheduler, FuzzingOptions
 from .scraper import Scraper
@@ -15,60 +15,12 @@ from . import warning_issuer
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-class CrawlerConfig:
-    def __init__(self):
-        self.set_default()
-
-    def set_default(self):
-        self.scrape_links = True
-        self.scrape_subdomains = True
-        self.scrape_phone_number = True
-        self.scrape_email = True
-        self.scrape_social_media = True
-        self.documents = {"doc", "docx", "ppt", "pptx", "pps", "xls", "xlsx", "csv", "odt", "odp", "pdf", "txt",
-                          "zip", "rar", "dmg", "exe", "apk", "bin", "rpm", "dpkg"}
-        self.obey_robots = True
-        self.allow_query = True
-        self.agent = arachnid_enums.Agent.FIREFOX.value
-        self.custom_str = None
-        self.custom_str_case_sensitive = False
-        self.custom_regex = None
-        self.default_delay = arachnid_enums.Delay.NONE.value
-        self.paths_list_file_loc = os.path.join(this_dir, "data/fuzz_list.txt")
-        self.subs_list_file_loc = os.path.join(this_dir, "data/subdomain_fuzz_list.txt")
-        self.fuzz_paths = False
-        self.fuzz_subs = False
-        self.blacklisted_directories = []
-
-    def set_stealth(self):
-        self.obey_robots = True
-        self.agent = arachnid_enums.Agent.GOOGLE.value
-        self.default_delay = arachnid_enums.Delay.HIGH.value
-        self.fuzz_paths = False
-        self.fuzz_subs = False
-
-    def set_aggressive(self):
-        self.obey_robots = False 
-        self.default_delay = arachnid_enums.Delay.NONE.value
-        self.fuzz_paths = True
-        self.fuzz_subs = True
-
-    def set_layout_only(self):
-        self.scrape_subdomains = False
-        self.scrape_phone_number = False 
-        self.scrape_email = False
-        self.scrape_social_media = False
-        self.documents = {}
-        self.custom_str = None
-        self.custom_regex = None
-
-
 class Crawler:
-    def __init__(self, seed, config=CrawlerConfig()):
+    def __init__(self, seed, configuration=config.CrawlerConfig()):
         seed = CrawlerURL(seed)
-        self.config = config
-        fuzzing_options = FuzzingOptions(config.paths_list_file_loc if config.fuzz_paths else None,
-                                         config.subs_list_file_loc if config.fuzz_subs else None)
+        self.config = configuration
+        fuzzing_options = FuzzingOptions(self.config.paths_list_file_loc if self.config.fuzz_paths else None,
+                                         self.config.subs_list_file_loc if self.config.fuzz_subs else None)
         self.schedule = Scheduler(seed, useragent=self.config.agent,
                                   fuzzing_options=fuzzing_options,
                                   respect_robots=self.config.obey_robots,
