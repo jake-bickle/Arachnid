f7dbdcb1c2ad91854b47fd23d355dbf5d64e12c5
jake-bickle <bickle.jake@gmail.com>
2019-12-16 20:15:49 -0800

    Remove PHPServer code from Arachnid class

--- arachnid/cli/arachnid.py
+++ arachnid/cli/arachnid.py
@@ -1,10 +1,6 @@
 import os
-import socket
-import webbrowser
 
 from cli import crawler
-from .php.finder import get_php_path
-from .php.server import PHPServer
 from .timewidgets import Timer
 from .arachnid_arg_parser import arachnid_cl_parser
 
@@ -33,15 +29,11 @@ class Arachnid:
         self.time_limit = ns.time_limit if ns.time_limit >= 0 else None
         self.file_write_timer = Timer()
         self.time_limit_timer = Timer()
-        self.server_address = self.get_unused_server_address()
-        self.server = PHPServer(output_dir, self.server_address, install_loc=get_php_path())
 
     def start(self):
         self.overwrite_output_files()
         self.file_write_timer.start()
         self.time_limit_timer.start()
-        if self.server:
-            self.open_server()
         while not self.is_done() and self.crawler.crawl_next():
             self.pages_crawled += 1
             if self.file_write_timer.elapsed() > 15:
@@ -50,10 +42,6 @@ class Arachnid:
 
     def is_done(self):
         return self.above_time_limit() or self.above_page_limit()
-
-    def open_server(self):
-        self.server.start()
-        webbrowser.open_new_tab(f"http://{self.server_address}")
 
     def overwrite_output_files(self):
         self.clear_file(output_file)
@@ -68,7 +56,6 @@ class Arachnid:
         self.crawler.finish()
         self.save()
         input("Crawl complete. Press ENTER to exit.")
-        self.server.stop()
 
     def above_time_limit(self):
         return self.time_limit_timer.elapsed() >= self.time_limit * 60 if self.time_limit else False
@@ -80,18 +67,3 @@ class Arachnid:
     def clear_file(file_loc):
         with open(file_loc, 'w') as f:
             f.write("")
-
-    @staticmethod
-    def get_unused_server_address():
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ip, port = default_ip.split(":")
-        port = int(port)
-        bound = False
-        while not bound:
-            try:
-                s.bind((ip, port))
-                bound = True
-            except OSError:
-                port += 1
-        s.close()
-        return f"{ip}:{str(port)}"
