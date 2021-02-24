bbbc9360e9d14b2c04c6e9d72a8fe66df41df440
jake-bickle <bickle.jake@gmail.com>
2021-02-23 19:16:39 -0800

    Gut out crawler and move code into arachnid

--- arachnid/arachnid.py
+++ arachnid/arachnid.py
@@ -3,7 +3,12 @@ from arachnid.crawler import crawler
 from arachnid.timewidgets import Timer
 from arachnid.arachnid_arg_parser import arachnid_cl_parser
 
-__version__ = "0.9.4"
+from arachnid.crawler import url_functions
+from arachnid.crawler import responseparser
+from arachnid.crawler import warning_issuer
+from arachnid.crawler.scraper import Scraper
+
+__version__ = "0.9.5.1"
 
 class Arachnid:
     def __init__(self, cli_args=None):
@@ -18,30 +23,76 @@ class Arachnid:
         self.pages_crawled = 0
         self.page_limit = ns.page_limit if ns.page_limit >= 0 else None
         self.time_limit = ns.time_limit if ns.time_limit >= 0 else None
-        self.file_write_timer = Timer()
         self.time_limit_timer = Timer()
         self.output_file = os.path.join(ns.output, "arachnid_results.json")
 
     def start(self):
-        self.save()
-        self.file_write_timer.start()
         self.time_limit_timer.start()
-        while not self.is_done() and self.crawler.crawl_next():
+        while not self.is_done() :
+            c_url, response = self.crawler.crawl_next()
             self.pages_crawled += 1
-            if self.file_write_timer.elapsed() > 15:
-                self.save()
+            if "content-type" in response.headers.keys():  # TODO What happens if there is no response type??
+                if "text/html" in response.headers["content-type"]:
+                    self._parse_page(r, c_url)
+                else:
+                    self._parse_document(r, c_url)
+
         self.close()
 
     def is_done(self):
-        return self.above_time_limit() or self.above_page_limit()
+        return self.above_time_limit() or self.above_page_limit() or not self.crawler.has_next_page()
 
-    def save(self):
-        with open(self.output_file, "w") as f:
-            f.write(self.crawler.dumps(indent=4, sort_keys=True))
+    def _parse_page(self, crawler_url, response):
+        """ 
+            Parses the page and writes information to output directory.
+            crawler_url is a CrawlerURL associated with the page.
+            response is a requests.response containing the page.
+        """
+        scraper = Scraper(response.text, "html.parser")
+        if self.config.scrape_email:
+            for email in scraper.find_all_emails():
+                self.output.add_email(email)
+        if self.config.scrape_phone_number:
+            for number in scraper.find_all_phones():
+                self.output.add_phone(number)
+        if self.config.scrape_social_media:
+            for social in scraper.find_all_social():
+                self.output.add_social(social)
+        if self.config.custom_regex:
+            for regex in scraper.find_all_regex(self.config.custom_regex):
+                self.output.add_custom_regex(regex)
+        found_urls = []
+        if self.config.scrape_links:
+            for page in scraper.find_all_http_refs():
+                page = page.strip().replace(" ", "%20")
+                url = url_functions.join_url(crawler_url.get_url(), page)
+                found_urls.append(url)
+        self.schedule.report_found_urls(found_urls)
+
+        backup_title = crawler_url.get_url_parts().path.split("/")[-1]
+        page_info = {"path": crawler_url.get_extension(),
+                     "title": scraper.title.string if scraper.title and scraper.title.string else backup_title,
+                     "custom_string_occurances": scraper.string_occurances(self.config.custom_str, self.config.custom_str_case_sensitive) if self.config.custom_str else None,
+                     "on_fuzz_list": crawler_url.is_fuzzed(),
+                     "on_robots": crawler_url.in_robots(),
+                     "code": response.status_code}
+        self.output.add_page(crawler_url.get_netloc(), page_info)
+
+    
+    def _parse_document(self, crawler_url, response):
+        """ 
+            Parses the page and writes information to output directory.
+            crawler_url is a CrawlerURL associated with the document.
+            response is a requests.response containing the document.
+        """
+        parser = responseparser.DocumentResponse(response, self.config.documents)
+        data = parser.extract()
+        self.schedule.report_found_urls([])
+        if data:
+            data["path"] = crawler_url.get_url_parts().path
+            self.output.add_document(crawler_url.get_netloc(), data)
 
     def close(self):
-        self.crawler.finish()
-        self.save()
         input("Crawl complete. Press ENTER to exit.")
 
     def above_time_limit(self):
